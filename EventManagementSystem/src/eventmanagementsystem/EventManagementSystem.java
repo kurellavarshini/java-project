d96987c0a2f67c1caebbeac8e425949e8eb4224e
kurellavarshini <59025397+kurellavarshini@users.noreply.github.com>
2021-05-07 19:58:16 +0530

    Add files via upload

---
+++ EventManagementSystem/src/eventmanagementsystem/EventManagementSystem.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventmanagementsystem;
+import java.awt.*;
+
+
+public class EventManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frame f1 = new LoginFrame();
+        f1.pack();
+        f1.setVisible(true);
+        f1.setSize(400,400);
+    }
+    
+}
